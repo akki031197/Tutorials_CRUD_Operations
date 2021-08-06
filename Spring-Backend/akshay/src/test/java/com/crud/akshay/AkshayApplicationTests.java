2fe498125dcef3d9a9d7cf4cb9a8fe0a1e4837ea
akki031197 <akshayjee6@gmail.com>
2021-08-06 21:08:10 +0530

    Complete Project On CRUD Operation in Angular and Spring Boot

---
+++ Spring-Backend/akshay/src/test/java/com/crud/akshay/AkshayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.akshay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkshayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
